7d4bc56fd25139f85af0886e406f76eafce11113
gsce7 <giorgos.sofronas03@gmail.com>
2026-09-02 12:18:19 +0300

    Create index.js

---
+++ genres/index.js
@@ -0,0 +1,17 @@
+import { fiction } from './fiction.js';
+import { fantasy } from './fantasy.js';
+import { mystery } from './mystery.js';
+import { romance } from './romance.js';
+import { horror } from './horror.js';
+import { nonfiction } from './nonfiction.js';
+
+export const GENRES = {
+  fiction,
+  fantasy,
+  mystery,
+  romance,
+  horror,
+  nonfiction
+};
+
+export const GENRE_ORDER = ['fiction', 'fantasy', 'mystery', 'romance', 'horror', 'nonfiction'];
