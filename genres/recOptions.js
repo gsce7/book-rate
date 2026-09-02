aaa7db9042a4a6a5167c4d47e3853ce63b680297
gsce7 <giorgos.sofronas03@gmail.com>
2026-09-02 12:21:08 +0300

    Create recOptions.js

---
+++ genres/recOptions.js
@@ -0,0 +1,7 @@
+export const REC_OPTIONS = [
+  { label: 'No', value: 0, feel: 'I would actively advise friends to skip this book.' },
+  { label: 'Unlikely', value: 1.25, feel: 'I might mention it offhand, but wouldn’t push anyone to read it.' },
+  { label: 'Maybe', value: 2.5, feel: 'I’d recommend it only to readers who love this specific niche.' },
+  { label: 'Yes', value: 3.75, feel: 'I would happily pass this along as a solid, enjoyable read.' },
+  { label: 'Highly', value: 5, feel: 'I will be raving about this to everyone who will listen.' }
+];
